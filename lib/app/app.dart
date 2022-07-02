2cb1aa3cfaf10007e5e188449dd908a0146b5f29
Wojtek <wojtek.koziol.pl@gmail.com>
2022-07-02 14:55:57 +0200

    Cleanup files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,19 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+import 'package:flash/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-export 'view/app.dart';
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppLocalizations.of(context).title,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+    );
+  }
+}
