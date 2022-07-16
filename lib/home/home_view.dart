c896ba8543a4f65f229cb4d636be6733c70d726b
Wojtek <wojtek.koziol.pl@gmail.com>
2022-07-16 15:48:36 +0200

    Create HomeView app bar

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,3 +1,6 @@
+import 'package:flash/config/constants.dart';
+import 'package:flash/home/widgets/app_logo.dart';
+import 'package:flash/home/widgets/profile_picture.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,8 +8,44 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    final scaffoldColor = Theme.of(context).scaffoldBackgroundColor;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(kPaddingL, kPaddingS, kPaddingL, 0),
+        child: Stack(
+          children: [
+            // ListView
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    AppLogo(),
+                    ProfilePicture(),
+                  ],
+                ),
+                const SizedBox(height: kPaddingM),
+                Text(
+                  'Dashboard',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Container(
+                  height: kPaddingL,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [scaffoldColor, scaffoldColor.withOpacity(0.1)],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
