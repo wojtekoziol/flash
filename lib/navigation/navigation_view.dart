148aeea1ec24d03e9921b31c959a220c0ec6ec17
Wojtek <wojtek.koziol.pl@gmail.com>
2022-07-16 13:51:09 +0200

    Fix home and profile pages not showing

--- lib/navigation/navigation_view.dart
+++ lib/navigation/navigation_view.dart
@@ -1,27 +1,33 @@
+import 'package:flash/home/home_view.dart';
 import 'package:flash/navigation/widgets/bottom_navbar.dart';
+import 'package:flash/profile/profile_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class NavigationView extends HookWidget {
   const NavigationView({super.key});
 
-  // final _pages = const [];
+  List<Widget> get _pages => const [HomeView(), ProfileView()];
 
   @override
   Widget build(BuildContext context) {
     final indexNotifier = useValueNotifier(0);
 
     return Scaffold(
-      // body: ValueListenableBuilder(
-      //   valueListenable: indexNotifier,
-      //   builder: (context, int index, child) {
-      //     return IndexedStack(
-      //       index: index,
-      //       children: _pages,
-      //     );
-      //   },
-      // ),
-      bottomNavigationBar: BottomNavbar(indexNotifier: indexNotifier),
+      body: Stack(
+        children: [
+          ValueListenableBuilder<int>(
+            valueListenable: indexNotifier,
+            builder: (context, index, child) {
+              return IndexedStack(
+                index: index,
+                children: _pages,
+              );
+            },
+          ),
+          BottomNavbar(indexNotifier: indexNotifier),
+        ],
+      ),
     );
   }
 }
