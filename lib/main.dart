976d5ec3cd5aacb40216b584fc13d9d1c1788dea
Wojtek <wojtek.koziol.pl@gmail.com>
2022-08-15 18:42:07 +0200

    Change folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:flash/app/app.dart';
+import 'package:flash/app.dart';
 import 'package:flash/bootstrap.dart';
 
 void main() {
