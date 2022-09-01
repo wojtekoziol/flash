b478955a153e440cd7aaa3b92ae0d936b69fb59c
Wojtek <wojtek.koziol.pl@gmail.com>
2022-09-01 15:14:23 +0200

    Add missing tests for FlashcardsCubit

--- test/data/bloc/flashcards/flashcards_cubit_test.dart
+++ test/data/bloc/flashcards/flashcards_cubit_test.dart
@@ -12,7 +12,7 @@ void main() {
       ),
       Flashcard(
         question: 'Question 2',
-        answer: 'Answer 3',
+        answer: 'Answer 2',
       ),
     ];
 
@@ -58,6 +58,79 @@ void main() {
         seed: FlashcardsState.finished,
         act: (cubit) => cubit.flip(),
         expect: List.empty,
+      );
+    });
+
+    group('know()', () {
+      blocTest<FlashcardsCubit, FlashcardsState>(
+        'emits [FlashcardsState.finished] when '
+        'there are no flashcards left.',
+        build: () => FlashcardsCubit(flashcards),
+        seed: () => FlashcardsState.answer(flashcards: [flashcards.first]),
+        act: (cubit) => cubit.know(),
+        expect: () => const [FlashcardsState.finished()],
+      );
+
+      blocTest<FlashcardsCubit, FlashcardsState>(
+        'emits [FlashcardsState.question] when '
+        'there are more flashcards in the deck.',
+        build: () => FlashcardsCubit(flashcards),
+        seed: () => const FlashcardsState.answer(flashcards: flashcards),
+        act: (cubit) => cubit.know(),
+        expect: () => [
+          FlashcardsState.question(
+            flashcards: List.from(flashcards)..removeAt(0),
+          ),
+        ],
+      );
+
+      blocTest<FlashcardsCubit, FlashcardsState>(
+        'emits [FlashcardsState.question] with shuffled flashcards when '
+        "there are flashcards in the deck that user marked as don't know.",
+        build: () => FlashcardsCubit(flashcards),
+        seed: () => const FlashcardsState.answer(
+          flashcards: flashcards,
+          index: 1,
+        ),
+        act: (cubit) => cubit.know(),
+        expect: () => [
+          FlashcardsState.question(flashcards: [flashcards.first]),
+        ],
+      );
+    });
+
+    group('dontKnow()', () {
+      blocTest<FlashcardsCubit, FlashcardsState>(
+        'emits [FlashcardsState.question] with next flashcard when '
+        "marked as don't know.",
+        build: () => FlashcardsCubit(flashcards),
+        seed: () => const FlashcardsState.answer(flashcards: flashcards),
+        act: (cubit) => cubit.dontKnow(),
+        expect: () => const [
+          FlashcardsState.question(flashcards: flashcards, index: 1),
+        ],
+      );
+
+      test(
+        'emits [FlashcardsState.question] with shuffled flashcards '
+        "marked as don't know.",
+        () {
+          final cubit = FlashcardsCubit(flashcards)
+            ..emit(const FlashcardsState.answer(
+              flashcards: flashcards,
+              index: 1,
+            ));
+
+          cubit.dontKnow();
+
+          final cards = cubit.state.whenOrNull(
+                question: (flashcards, _) => flashcards,
+              ) ??
+              [];
+          expect(flashcards.any(cards.contains), true);
+          expect(cards.any(flashcards.contains), true);
+          expect(cards.length, flashcards.length);
+        },
       );
     });
   });
