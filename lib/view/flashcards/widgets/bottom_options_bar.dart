b068a6ddd410f59857a7cfa1c91c381a74e823c3
Wojtek <wojtek.koziol.pl@gmail.com>
2022-09-01 16:25:22 +0200

    Add bottom options bar buttons animations

--- lib/view/flashcards/widgets/bottom_options_bar.dart
+++ lib/view/flashcards/widgets/bottom_options_bar.dart
@@ -2,9 +2,10 @@ import 'package:flash/config/constants.dart';
 import 'package:flash/data/bloc/flashcards/flashcards_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:unicons/unicons.dart';
 
-class BottomOptionsBar extends StatelessWidget {
+class BottomOptionsBar extends HookWidget {
   const BottomOptionsBar({super.key});
 
   @override
@@ -12,35 +13,64 @@ class BottomOptionsBar extends StatelessWidget {
     final theme = Theme.of(context);
     final cubit = context.read<FlashcardsCubit>();
 
-    return BlocBuilder<FlashcardsCubit, FlashcardsState>(
-      builder: (context, state) {
-        return state.when(
-          question: (_, __) => _Button(
-            onTap: cubit.flip,
-            color: theme.floatingActionButtonTheme.backgroundColor,
-            child: Text(
-              'Reveal Answer',
-              style: theme.textTheme.bodyText2,
+    final questionAnimController =
+        useAnimationController(duration: kShortAnimDuration)..forward();
+    final answerAnimController =
+        useAnimationController(duration: kShortAnimDuration);
+    final bounceTween = CurveTween(curve: kDefaultAnimCurve);
+
+    return BlocListener<FlashcardsCubit, FlashcardsState>(
+      listener: (_, state) {
+        state.whenOrNull(
+          question: (_, __) {
+            answerAnimController.reverse().then(
+                  (_) => questionAnimController.forward(),
+                );
+          },
+          answer: (_, __) {
+            questionAnimController.reverse().then(
+                  (_) => answerAnimController.forward(),
+                );
+          },
+        );
+      },
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          ScaleTransition(
+            scale: bounceTween.animate(questionAnimController),
+            child: _Button(
+              onTap: cubit.flip,
+              color: theme.floatingActionButtonTheme.backgroundColor,
+              child: Text(
+                'Reveal Answer',
+                style: theme.textTheme.bodyText2,
+              ),
             ),
           ),
-          answer: (_, __) => Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _Button(
-                onTap: cubit.dontKnow,
-                color: theme.colorScheme.secondary,
-                child: const Icon(UniconsLine.question),
+              ScaleTransition(
+                scale: bounceTween.animate(answerAnimController),
+                child: _Button(
+                  onTap: cubit.dontKnow,
+                  color: theme.colorScheme.secondary,
+                  child: const Icon(UniconsLine.question),
+                ),
               ),
-              _Button(
-                onTap: cubit.know,
-                color: theme.floatingActionButtonTheme.backgroundColor,
-                child: const Icon(UniconsLine.check),
+              ScaleTransition(
+                scale: bounceTween.animate(answerAnimController),
+                child: _Button(
+                  onTap: cubit.know,
+                  color: theme.floatingActionButtonTheme.backgroundColor,
+                  child: const Icon(UniconsLine.check),
+                ),
               ),
             ],
           ),
-          finished: SizedBox.shrink,
-        );
-      },
+        ],
+      ),
     );
   }
 }
